a7547ab391979e281360e19872183e9d434798b3
larjo <lars.o.baker@gmail.com>
2015-01-21 09:21:09 +0100

    bug fix in nonadmin-install.bat

--- nonadmin-install.bat
+++ nonadmin-install.bat
@@ -1,4 +1,4 @@
 git clone https://github.com/Shougo/neobundle.vim %USERPROFILE%/vimfiles/bundle/neobundle.vim
 copy mswin.vim %USERPROFILE%\mswin.vim 
-copy mswin.vim %USERPROFILE%\_vimrc
+copy _vimrc %USERPROFILE%\_vimrc
 gvim
