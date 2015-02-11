7340aa2b97d703190ca0a4f2b080f929b44d8038
larjo <lars.o.baker@gmail.com>
2015-02-11 08:58:59 +0100

    non admin install

--- nonadmin-install.bat
+++ nonadmin-install.bat
@@ -1,4 +1,4 @@
-git clone https://github.com/Shougo/neobundle.vim %USERPROFILE%/vimfiles/bundle/neobundle.vim
-copy mswin.vim %USERPROFILE%\mswin.vim 
-copy _vimrc %USERPROFILE%\_vimrc
-gvim
+git clone https://github.com/Shougo/neobundle.vim vimfiles/bundle/neobundle.vim
+copy /Y dotfiles\mswin.vim mswin.vim 
+copy /Y dotfiles\_vimrc _vimrc
+gvim +NeoBundleInstall +qall
